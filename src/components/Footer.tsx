090542b929b7b2bd57d0496d81184fff700ded55
iceniveth <ken11zer01@gmail.com>
2023-04-25 22:01:38 +0800

    Fix ESLint warnings

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,22 +13,22 @@ const Footer = () => {
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
+            <a href="#about" className="mr-4 hover:underline md:mr-6">
               About
             </a>
           </li>
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
+            <a href="#privacy-policy" className="mr-4 hover:underline md:mr-6">
               Privacy Policy
             </a>
           </li>
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
+            <a href="#licensing" className="mr-4 hover:underline md:mr-6">
               Licensing
             </a>
           </li>
           <li>
-            <a href="#" className="hover:underline">
+            <a href="#contact" className="hover:underline">
               Contact
             </a>
           </li>
