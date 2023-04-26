158c770e051c33446ff2c57dd868b8c48853af40
Josephine Flogoso <josephine.flogoso@antiquespride.edu.ph>
2023-04-26 18:59:34 +0800

    update by josephine

--- src/pages/josephine/index.tsx
+++ src/pages/josephine/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
 import Quiz from "./components/Quiz";
 import { QuizProvider } from "./contexts/quiz";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <QuizProvider>
-      <Quiz />
-    </QuizProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const App: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <QuizProvider>
+        <Quiz />
+      </QuizProvider>
+    </React.StrictMode>
+  );
+};
 
+export default App;
