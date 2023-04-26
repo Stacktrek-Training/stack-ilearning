efb7ca92ec0364d4cbf6bd71487ff7145e223b74
iceniveth <ken11zer01@gmail.com>
2023-04-26 21:28:45 +0800

    Fix code formatting issue and lint warnings

--- src/pages/michael/Quiz1.tsx
+++ src/pages/michael/Quiz1.tsx
@@ -6,24 +6,25 @@ const Quiz1 = () => {
     <div>
       <Navigationbar />
       <center>
-      <div className="pt-20 w-5/6 ">
-        <a
-          href="#"
-          className=" w-5/6 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-        >
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.  Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order. Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-        </a>
-      </div>
+        <div className="pt-20 w-5/6 ">
+          <a
+            href="#noteworthy-techonology-acquisitions-2021"
+            className=" w-5/6 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          >
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order. Here are the biggest
+              enterprise technology acquisitions of 2021 so far, in reverse
+              chronological order. Here are the biggest enterprise technology
+              acquisitions of 2021 so far, in reverse chronological order. Here
+              are the biggest enterprise technology acquisitions of 2021 so far,
+              in reverse chronological order.
+            </p>
+          </a>
+        </div>
       </center>
     </div>
   );
