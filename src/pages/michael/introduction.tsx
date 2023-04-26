b38b750f3649afb49f2407791ee263de5ae0b95c
John Michael <57973306+jmlagrana@users.noreply.github.com>
2023-04-26 16:23:56 +0800

    M-update

--- src/pages/michael/introduction.tsx
+++ src/pages/michael/introduction.tsx
@@ -1,18 +1,116 @@
 import React from "react";
 import Navigatiobar from "../../components/Navigationbar";
+import { Link } from 'react-router-dom';
+
+
 const introduction = () => {
   return (
     <div>
       <Navigatiobar />
-      <a href="#" className="block min-w-min w-3/4 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-auto mt-6">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order
-        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order
-        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order
-        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order
-        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order
-        </p>
-      </a>
+      <div>
+        <a
+          href="#"
+          className="block overflow-y-scroll min-w-min max-h-100 w-3/4 h-96 p-6  bg-white border
+           border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700
+            dark:hover:bg-gray-700 mx-auto mt-6 mb-20"
+        >
+          <center>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+          </center>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order Here are the biggest enterprise
+            technology acquisitions of 2021 so far, in reverse chronological
+            order Here are the biggest enterprise technology acquisitions of
+            2021 so far, in reverse chronological order Here are the biggest
+            enterprise technology acquisitions of 2021 so far, in reverse
+            chronological order Here are the biggest enterprise technology
+            acquisitions of 2021 so far, in reverse chronological order Here are
+            the biggest enterprise technology acquisitions of 2021 so far, in
+            reverse chronological order Here are the biggest enterprise
+            technology acquisitions of 2021 so far, in reverse chronological
+            order Here are the biggest enterprise technology acquisitions of
+            2021 so far, in reverse chronological order Here are the biggest
+            enterprise technology acquisitions of 2021 so far, in reverse
+            chronological order Here are the biggest enterprise technology
+            acquisitions of 2021 so far, in reverse chronological order Here are
+            the biggest enterprise technology acquisitions of 2021 so far, in
+            reverse chronological order Here are the biggest enterprise
+            technology acquisitions of 2021 so far, in reverse chronological
+            order Here are the biggest enterprise technology acquisitions of
+            2021 so far, in reverse chronological order Here are the biggest
+            enterprise technology acquisitions of 2021 so far, in reverse
+            chronological order Here are the biggest enterprise technology
+            acquisitions of 2021 so far, in reverse chronological order Here are
+            the biggest enterprise technology acquisitions of 2021 so far, in
+            reverse chronological order Here are the biggest enterprise
+            technology acquisitions of 2021 so far, in reverse chronological
+            order Here are the biggest enterprise technology acquisitions of
+            2021 so far, in reverse chronological order Here are the biggest
+            enterprise technology acquisitions of 2021 so far, in reverse
+            chronological order Here are the biggest enterprise technology
+            acquisitions of 2021 so far, in reverse chronological order Here are
+            the biggest enterprise technology acquisitions of 2021 so far, in
+            reverse chronological order Here are the biggest enterprise
+            technology acquisitions of 2021 so far, in reverse chronological
+            order Here are the biggest enterprise technology acquisitions of
+            2021 so far, in reverse chronological order Here are the biggest
+            enterprise technology acquisitions of 2021 so far, in reverse
+            chronological order Here are the biggest enterprise technology
+            acquisitions of 2021 so far, in reverse chronological order Here are
+            the biggest enterprise technology acquisitions of 2021 so far, in
+            reverse chronological order Here are the biggest enterprise
+            technology acquisitions of 2021 so far, in reverse chronological
+            order Here are the biggest enterprise technology acquisitions of
+            2021 so far, in reverse chronological order Here are the biggest
+            enterprise technology acquisitions of 2021 so far, in reverse
+            chronological order Here are the biggest enterprise technology
+            acquisitions of 2021 so far, in reverse chronological order Here are
+            the biggest enterprise technology acquisitions of 2021 so far, in
+            reverse chronological order Here are the biggest enterprise
+            technology acquisitions of 2021 so far, in reverse chronological
+            order Here are the biggest enterprise technology acquisitions of
+            2021 so far, in reverse chronological order Here are the biggest
+            enterprise technology acquisitions of 2021 so far, in reverse
+            chronological order Here are the biggest enterprise technology
+            acquisitions of 2021 so far, in reverse chronological order Here are
+            the biggest enterprise technology acquisitions of 2021 so far, in
+            reverse chronological order Here are the biggest enterprise
+            technology acquisitions of 2021 so far, in reverse chronological
+            order Here are the biggest enterprise technology acquisitions of
+            2021 so far, in reverse chronological order Here are the biggest
+            enterprise technology acquisitions of 2021 so far, in reverse
+            chronological order Here are the biggest enterprise technology
+            acquisitions of 2021 so far, in reverse chronological order Here are
+            the biggest enterprise technology acquisitions of 2021 so far, in
+            reverse chronological order Here are the biggest enterprise
+            technology acquisitions of 2021 so far, in reverse chronological
+            order Here are the biggest enterprise technology acquisitions of
+            2021 so far, in reverse chronological order Here are the biggest
+            enterprise technology acquisitions of 2021 so far, in reverse
+            chronological order Here are the biggest enterprise technology
+            acquisitions of 2021 so far, in reverse chronological order Here are
+            the biggest enterprise technology acquisitions of 2021 so far, in
+            reverse chronological order
+            <br />
+            <br />
+          </p>
+          <center>
+            <Link to="/MichaelQuiz1">
+          <button 
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4
+             focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600
+             dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-block w-auto h-auto mx-auto"
+          >
+            NEXT
+          </button>
+          </Link>
+          </center>
+        </a>
+      </div>
     </div>
   );
 };
