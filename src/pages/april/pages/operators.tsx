5bb2c9438de74ddf909755fc57e5288a74345784
Garfin-AprilJane <garfinapriljane@gmail.com>
2023-05-08 09:56:09 +0800

    added folders/files

---
+++ src/pages/april/pages/operators.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+function Operators() {
+  return (
+    <div className="bg-gradient-to-b from-white to-blue-500 h-screen w-screen">
+      <div className="flex items-center">
+        <img
+          src={logo}
+          alt="Logo"
+          className="h-50 w-50 object-contain mt-6 ml-6"
+        />
+      </div>
+      <hr className="border-3 border-black my-4 mx-15" />
+    </div>
+  )
+}
+
+export default Operators;
