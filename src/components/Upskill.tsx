dc3bc630cf5817bd0f5096d081e9c04cbf1aae94
Garfin-AprilJane <garfinapriljane@gmail.com>
2023-05-19 14:58:27 +0800

    Removed concept boxes and fix some issues

--- src/components/Upskill.tsx
+++ src/components/Upskill.tsx
@@ -20,11 +20,11 @@ const Proceed = () => {
 
       <hr className="border-3 border-black my-[10px] mx-0 w-full" />
 
-      <div className="space-y-15 space-x-2 pt-0 grid grid-cols-3 gap-3 p-20">
+      <div className="space-y-15 space-x-2 pt-[-35px] grid grid-cols-3 gap-3 p-20 items-center justify-center">
         {/*SQL*/}
         <div
           className="text-white text-2xl font-serif font-bold inline-block rounded-3xl 
-  shadow-lg shadow-bottom bg-[#C1D0B5] border-transparent border-2 p-3 h-60 mt-14"
+  shadow-lg shadow-bottom bg-[#FFFFE8] border-transparent border-2 p-3 h-60 mt-14"
         >
           <h1 className="text-center flex justify-center items-center h-full">
             <a href="/SQL" className="text-black hover:text-blue-900">
@@ -36,7 +36,7 @@ const Proceed = () => {
         {/*Concept 2*/}
         <div
           className="text-white text-2xl font-serif font-bold inline-block rounded-3xl 
-    shadow-lg shadow-bottom bg-[#FFF7D4] border-transparent border-2 p-3 h-60 mt-14"
+    shadow-lg shadow-bottom bg-[#FFFFE8] border-transparent border-2 p-3 h-60 mt-14"
         >
           <h1 className="text-center flex justify-center items-center h-full">
             <a href="/Concept 2" className="text-black hover:text-blue-900">
@@ -48,48 +48,11 @@ const Proceed = () => {
         {/*Concept 3*/}
         <div
           className="text-white text-2xl font-serif font-bold inline-block rounded-3xl 
-    shadow-lg shadow-bottom bg-[#DDFFBB] border-transparent border-2 p-3 h-60 mt-14"
+    shadow-lg shadow-bottom bg-[#FFFFE8] border-transparent border-2 p-3 h-60 mt-14"
         >
           <h1 className="text-center flex justify-center items-center h-full">
             <a href="/Concept 3" className="text-black hover:text-blue-900">
               Concept 3
-            </a>
-          </h1>
-        </div>
-
-
-        {/*Concept 4*/}
-        <div
-          className="text-white text-2xl font-serif font-bold inline-block rounded-3xl 
-  shadow-lg shadow-bottom bg-[#57C5B6] border-transparent border-2 p-3 h-60 mt-5"
-        >
-          <h1 className="text-center flex justify-center items-center h-full">
-            <a href="/Concept 4" className="text-black hover:text-blue-900">
-              Concept 4
-            </a>
-          </h1>
-        </div>
-
-        {/*Concept 5*/}
-        <div
-          className="text-white text-2xl font-serif font-bold inline-block rounded-3xl 
-  shadow-lg shadow-bottom bg-[#BFACE2] border-transparent border-2 p-3 h-60 mt-5"
-        >
-          <h1 className="text-center flex justify-center items-center h-full">
-            <a href="/Concept 5" className="text-black hover:text-blue-900">
-              Concept 5
-            </a>
-          </h1>
-        </div>
-
-        {/*Concept 6*/}
-        <div
-          className="text-white text-2xl font-serif font-bold inline-block rounded-3xl 
-  shadow-lg shadow-bottom bg-[#FFFF9D] border-transparent border-2 p-3 h-60 mt-5"
-        >
-          <h1 className="text-center flex justify-center items-center h-full">
-            <a href="/Concept 6" className="text-black hover:text-orange-500">
-              Concept 6
             </a>
           </h1>
         </div>
