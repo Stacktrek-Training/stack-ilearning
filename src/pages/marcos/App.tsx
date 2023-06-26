9cf6ea689e2c1454282b7480284e2ee376ce7460
Marcos <marcoshabacon77@gmail.com>
2023-06-26 12:07:10 +0800

    added quiz game and quizcomponent

---
+++ src/pages/marcos/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import QuizComponent from "./quizcomponents";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <QuizComponent />
+    </div>
+  );
+};
+
+export default App;
